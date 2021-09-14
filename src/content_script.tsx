1f11e8e4eebe86e0eedfa59c6e425c443f739627
Coder_Q <1282117217@qq.com>
2021-09-14 22:58:34 +0800

    init: 🌈初始化项目

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,22 @@
+let time: NodeJS.Timeout | null = null;
+let count = 0;
+
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.color) {
-    console.log("Receive color = " + msg.color);
-    document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
-  } else {
-    sendResponse("Color message is none.");
+  if (msg.time && count === 0) {
+    time = null;
+    let to = window.pageYOffset;
+      time = setInterval(() => {
+      ++to
+      window.scrollTo(0, to);
+    }, msg.time)
+    count+=1;
+    console.log("接收成功", msg.time, typeof msg.time, time);
+  }
+
+  if(msg.stop && count > 0) {
+    clearInterval(time as NodeJS.Timeout)
+    time = null;
+    console.log("结束",time)
+    count = 0;
   }
 });
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,51 +1,75 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import './assets/popup.css'
 
 const Popup = () => {
-  const [count, setCount] = useState(0);
-  const [currentURL, setCurrentURL] = useState<string>();
+  const [scrollTime, setScrollTime] = useState<number>(100);
 
-  useEffect(() => {
-    chrome.browserAction.setBadgeText({ text: count.toString() });
-  }, [count]);
+  const getSliderValue = (value: any) => {
+    console.log(value);
+    setScrollTime(value)
+  }
+  // const [count, setCount] = useState(0);
+  // const [currentURL, setCurrentURL] = useState<string>();
 
-  useEffect(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      setCurrentURL(tabs[0].url);
-    });
-  }, []);
+  // useEffect(() => {
+  //   chrome.browserAction.setBadgeText({ text: count.toString() });
+  // }, [count]);
 
-  const changeBackground = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  // useEffect(() => {
+  //   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  //     setCurrentURL(tabs[0].url);
+  //   });
+  // }, []);
+
+  interface ChromeQuery {
+    active: boolean,
+    currentWindow: boolean
+  }
+
+  const pageScrolling = () => {
+    chrome.tabs.query({ active: true, currentWindow: true } as ChromeQuery, (tabs) => {
       const tab = tabs[0];
       if (tab.id) {
-        chrome.tabs.sendMessage(
-          tab.id,
-          {
-            color: "#555555",
-          },
-          (msg) => {
-            console.log("result message:", msg);
-          }
-        );
+        chrome.tabs.sendMessage(tab.id, { time: scrollTime }, (msg) => {
+            console.log("页面通信回来的数据", msg);
+        });
       }
     });
   };
 
+  const stopScroll = () => {
+    chrome.tabs.query({ active: true, currentWindow: true } as ChromeQuery, (tabs) => {
+      const tab = tabs[0];
+      if(tab.id) {
+        chrome.tabs.sendMessage(tab.id, { stop: true }, msg => {
+          console.log("页面通信回来的回调(禁止滚动)", msg)
+        })
+      }
+    })
+  }
+
   return (
-    <>
-      <ul style={{ minWidth: "700px" }}>
-        <li>Current URL: {currentURL}</li>
-        <li>Current Time: {new Date().toLocaleTimeString()}</li>
-      </ul>
-      <button
-        onClick={() => setCount(count + 1)}
-        style={{ marginRight: "5px" }}
-      >
-        count up
-      </button>
-      <button onClick={changeBackground}>change background</button>
-    </>
+      <div className="popup-main">
+        <h4>调节速度</h4>
+        <Slider max={110} 
+                defaultValue={60} 
+                marks={{ 
+                    10: 10,
+                    30: 30, 
+                    50: 50, 
+                    70: 70,
+                    90: 90,
+                    110: 110
+                }} 
+                onChange={getSliderValue} 
+                step={null}/>
+        <div className="scroll-btn" onClick={pageScrolling}>开始滚动</div>
+        <div className="unScroll-btn" onClick={stopScroll}>取消滚动</div>
+      </div>
+      
   );
 };
 
